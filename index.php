683fbdb58b265457a2767294bdf5efc550504e95
kai <coelacanth2505@gmail.com>
2021-01-10 15:50:14 +0900

    no message

--- index.php
+++ index.php
@@ -1,23 +1,61 @@
+<?php
+?>
+<!-- ①フォームの説明 -->
+<!-- ②$_FILEの確認 -->
+<!-- ③バリデーション -->
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>php 気づけばプロ並み</title>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>アップロードフォーム</title>
 </head>
+<style>
+  body {
+    padding: 30px;
+    margin: 0 auto;
+    width: 50%;
+  }
+
+  textarea {
+    width: 98%;
+    height: 60px;
+  }
+
+  .file-up {
+    margin-bottom: 10px;
+  }
+
+  .submit {
+    text-align: right;
+  }
+
+  .btn {
+    display: inline-block;
+    border-radius: 3px;
+    font-size: 18px;
+    background: #67c5ff;
+    border: 2px solid #67c5ff;
+    padding: 5px 10px;
+    color: #fff;
+    cursor: pointer;
+  }
+</style>
+
 <body>
-  スタッフ追加<br />
-  <br />
-  <form method="post" action="staff_add_check.php">
-  スタッフ名を入力してください。<br />
-  <input type="text" name="name" style="width:200px"><br />
-  パスワードを入力してください<br />
-  <input type="password" name="pass" style="width:100px"><br />
-  パスワードをもう一度入力してください<br />
-  <input type="password" name="pass2" style="width:100px"><br />
-  <br />
-  <input type="button" onclick="history.back()" value="戻る">
-  <input type="submit" value="OK">
+  <form enctype="multipart/form-data" action="./file_upload.php" method="POST">
+    <div class="file-up">
+      <input type="hidden" name="MAX_FILE_SIZE" value="5" />
+      <input name="img" type="file" accept="image/*" />
+    </div>
+    <div>
+      <textarea name="caption" placeholder="キャプション（140文字以下）" id="caption"></textarea>
+    </div>
+    <div class="submit">
+      <input type="submit" value="送信" class="btn" />
+    </div>
   </form>
 </body>
+
 </html>
